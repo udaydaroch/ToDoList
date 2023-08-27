1f2a163369f92066f090e173337e2fc06763cd23
uda12 <uda12@uclive.ac.nz>
2023-08-27 22:06:39 +1200

    added css to make the table look a little better

--- LandingPage.js
+++ LandingPage.js
@@ -5,21 +5,27 @@ const table = document.getElementById("table");
 
 
 function gettaskField() {
-  console.log("triggered");
   
   if (startTime.value >= endTime.value)
   {
     alert("invalid start and end time");
   }
   else {
+    
     const newRow = table.insertRow();
     const taskrow = newRow.insertCell(0);
     const DurationRow = newRow.insertCell(1);
     const deleteRow = newRow.insertCell(2);
 
     taskrow.innerHTML = taskInputField.value;
-    DurationRow.innerHTML = startTime.value + "to" + endTime.value;
-    deleteRow.innerHTML = "delete task";
+    taskrow.classList.add("table-fond");
 
+    DurationRow.innerHTML = startTime.value + " to " + endTime.value;
+    DurationRow.classList.add("table-fond");
+
+    deleteRow.innerHTML = "delete task";
+    deleteRow.classList.add("table-fond");
+    
+    
   }
 }
